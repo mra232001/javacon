1733a3bac6775ff00aea4ac064dcb3f0c2cbfbd2
lam <dinhtruonglam001@gmail.com>
2020-12-31 15:41:06 +0300

    3

--- src/lam/noway.java
+++ src/lam/noway.java
@@ -1,7 +1,9 @@
+package lam;
+
 import java.util.Scanner;
 
 public class noway {
     public static void main(String [] args){
-        Scanner scanner
+        Scanner scanner = new Scanner(System.in);
     }
 }
